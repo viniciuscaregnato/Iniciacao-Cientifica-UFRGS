79c8a3c0d3266803ec3d22adb122ddcb9987d1fd
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-04-18 14:23:42 -0300

    data_prep.R

--- 2.data_prep.R
+++ 2.data_prep.R
@@ -1,26 +1,36 @@
-data_prep <- function(data, variable, insample = 1) {
+data_prep <- function(df, target_var, independents_var, ART=2, horizon=1) {
   
-  #insample: tamanho da amostra insample (de 0.0 a 1.0)
-  #dados: arquivo analisado
-  #varible: variavel dependente
+  require(dplyr)
   
-  if (insample==1){
-    from <- 1
-    to <- nrow(dados)
-  }
-  else 
-  { 
-    from <- 1
-    to <- nrow(dados)*insample
-    
+  # df: janela recebida de rolling_window
+  # target_var: variavel alvo
+  # independents_var: variavel dependente
+  # AutoRegressive Terms: numero de defasagens incluidas no modelo
+  
+  for (i in seq_len(ART)) {
+    lag_name <- paste0(target_var, "_lag", i)
+    df[[lag_name]] <- dplyr::lag(df[[target_var]], n = i)
   }
   
   
-  data_sample <- dados[from:to,]
-  y <- data_sample[,variable]
-  x <- data_sample[,setdiff(names(data_sample),variable)]
+  features <- c(independents_var,
+                paste0(target_var, "_lag", seq_len(ART)))
   
-  return(list(x=x, y=y, dados_prontos=data_sample))
+  df[[paste0(target_var, "_lead")]] <- dplyr::lead(df[[target_var]], n = horizon)
+  
+  df_clean <- df %>%
+    filter(if_all(all_of(features), ~ !is.na(.x)),
+           !is.na(.data[[paste0(target_var, "_lead")]]))
+  
+  Xin  <- as.matrix(df_clean[, features])
+  yin  <- df_clean[[paste0(target_var, "_lead")]]
+  Xout <- tail(Xin, n = horizon)            
+  Yin_out <- tail(df_clean[[target_var]], horizon)
+  
+  return(list(
+    Xin       = Xin,        # preditoras in-sample
+    yin       = yin,        # target in-sample (lead de Y)
+    Xout      = Xout,       # preditoras out-of-sample
+    Yout_real = Yin_out     # valor real para comparaçao
+  ))
 }
-
-
