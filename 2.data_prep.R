b3229f40d86c90ebacd604b0fb75dfe0fc6462b2
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-04-18 18:04:47 -0300

    Update 2.data_prep.R

--- 2.data_prep.R
+++ 2.data_prep.R
@@ -1,24 +1,35 @@
-dataprep <- function(df, target_var, ART) {
+dataprep <- function(df, target_var, ART, horizon=1) {
   
   # df: janela recebida de rolling window
   # target_var: variável dependente
   # ART: quantidade de defasagens (autoregressive terms)
+  # horizon: horizonte de previsao
   
-  y_art <- embed(df[[target_var]], ART)
-  df_trimmed <- df[ART:nrow(df), ]
+  ## criando colunas de defasagens separadas do resto ##
   
-  # cria colunas de defasagens com nomes claros
-  colnames(y_art) <- paste0(target_var, "_t-", 1:ART)
+  y_art <- embed(df[[target_var]], ART)                   # cria df de defasagens de y
+  colnames(y_art) <- paste0(target_var, "_t-", 1:ART)     # nomeia as colunas de y_t-..
   
-  # remove coluna target_var de df_trimmed para montar Xin/Xout
-  X_all <- df_trimmed[, setdiff(names(df_trimmed), target_var)]
+  ## ajustando o df geral ##
   
-  # junta defasagens com as demais variáveis independentes
-  X_full <- cbind(X_all, y_art)
+  df_trimmed <- df[ART:nrow(df), ]                        # exclue as linhas perdidas ao aplicar ART
   
-  Xin  <- X_full[1:(nrow(X_full) - 1), ]
-  Xout <- X_full[nrow(X_full), , drop = FALSE]
-  Yin  <- df_trimmed[[target_var]][2:nrow(df_trimmed)]  # target deslocado
+  ## criando daframe de regressores ##
+  
+   
+  X_all <- df_trimmed[, setdiff(names(df_trimmed), target_var)]  # remove coluna target_var de df_trimmed para montar Xin/Xout
+  
+  ## criando dataframe completo com todas as informações ##
+  
+  X_full <- cbind(X_all, y_art)                           # junta defasagens com as demais variáveis independentes
+  
+  
+  ## concluindo cada parte 
+  
+  Xin  <- X_full[1:(nrow(X_full)-horizon),]
+  Xout <- X_full[nrow(X_full)-horizon+1,]
+  Yin  <- df_trimmed[[target_var]][(1+horizon):(nrow(Xin)+horizon)]  
   
   return(list(Xin = Xin, Yin = Yin, Xout = Xout, df_final = cbind(df_trimmed, y_art)))
 }
+
