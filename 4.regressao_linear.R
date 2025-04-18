7f2c3663593c4a7ad45fc6fd1dd8e9ceb4356f48
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-04-18 18:10:46 -0300

    Update 4.regressao_linear.R

--- 4.regressao_linear.R
+++ 4.regressao_linear.R
@@ -1,4 +1,4 @@
-regressao_linear <- function(df, target_var, ART){
+regressao_linear <- function(df, target_var, ART, horizon =1){
   
   
   prep_data <- dataprep(df, target_var, ART)
@@ -6,12 +6,13 @@ regressao_linear <- function(df, target_var, ART){
   yin  <- prep_data$Yin
   Xin  <- prep_data$Xin
   Xout <- prep_data$Xout
+  df_final <- prep_data$df_final
   
   modelo <- lm(yin ~ ., data=Xin)
   
   previsao <- predict(modelo, newdata = Xout)
   
   
-    resultados <-  return(previsao)
+    resultados <-  return(list(previsao, df_final))
   
 }
