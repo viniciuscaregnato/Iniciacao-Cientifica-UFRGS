b094f198c0d4b638188a95809d0297122bb0d61f
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-04-19 00:03:58 -0300

    Update 2.data_prep.R

--- 2.data_prep.R
+++ 2.data_prep.R
@@ -5,26 +5,26 @@ dataprep <- function(df, target_var, ART, horizon=1) {
   # ART: quantidade de defasagens (autoregressive terms)
   # horizon: horizonte de previsao
   
-  ## criando colunas de defasagens separadas do resto ##
+  #### criando colunas de defasagens separadas do resto ####
   
   y_art <- embed(df[[target_var]], ART)                   # cria df de defasagens de y
   colnames(y_art) <- paste0(target_var, "_t-", 1:ART)     # nomeia as colunas de y_t-..
   
-  ## ajustando o df geral ##
+  #### ajustando o df geral ####
   
   df_trimmed <- df[ART:nrow(df), ]                        # exclue as linhas perdidas ao aplicar ART
   
-  ## criando daframe de regressores ##
+  #### criando daframe de regressores ####
   
    
   X_all <- df_trimmed[, setdiff(names(df_trimmed), target_var)]  # remove coluna target_var de df_trimmed para montar Xin/Xout
   
-  ## criando dataframe completo com todas as informações ##
+  #### criando dataframe completo com todas as informações ####
   
   X_full <- cbind(X_all, y_art)                           # junta defasagens com as demais variáveis independentes
   
   
-  ## concluindo cada parte 
+  #### concluindo cada parte ####
   
   Xin  <- X_full[1:(nrow(X_full)-horizon),]
   Xout <- X_full[nrow(X_full)-horizon+1,]
