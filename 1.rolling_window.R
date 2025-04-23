34b2a52b184917facd8878470f9a83085a9b9ce4
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-04-23 14:35:11 -0300

    Update 1.rolling_window.R

--- 1.rolling_window.R
+++ 1.rolling_window.R
@@ -1,11 +1,13 @@
 rolling_window <- function(df, target_var, ART, horizon = 1, n_predicts = 20, fnct) {
+
+  # n_predicts: numero de observações Yout ou Xout
   
   previsoes <- vector("list", n_predicts)                              #cria uma lista vazia de tamanho n_predicts
   
   for (i in 0:(n_predicts - 1)) {
     
     
-    df_window <- df[(1 + i):(nrow(df) - n_predicts + i + horizon), ]
+    df_window <- df[(1 + i):(nrow(df) - n_predicts + i + horizon), ]   # pega do primeiro Xin ate o ultimo Yin de cada "i+1"° janela
     
     
     resultado <- fnct(df_window, target_var, ART, horizon)
