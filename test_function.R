d260746e85e3b2d4e80c55c5624a5f3554c6db30
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-05-12 15:26:24 -0300

    Update test_function.R

--- test_function.R
+++ test_function.R
@@ -3,16 +3,15 @@ regressao_linear <- function(df_window, target_var, ART, horizon){
   
   prep_data <- dataprep(df_window, target_var, ART, horizon)
   
-  yin  <- prep_data$Yin
-  Xin  <- prep_data$Xin
-  Xout <- prep_data$Xout
-  df_window_final <- prep_data$df_window_final
+  data_in <- prep_data$data_in                      # trago data.frame data_in para poder manipulá-lo
   
-  modelo <- lm(yin ~ ., data=Xin)
+  Xout <- prep_data$Xout                            # trago o Xout para aplicá-lo
+  
+  modelo <- lm(Yin ~ ., data=data_in)
   
   previsao <- predict(modelo, newdata = Xout)
   
   
-  resultados <-  return(list(previsao = previsao, yin = yin, Xin = Xin, Xout = Xout))
+  resultados <-  return(previsao = previsao)
   
 }
