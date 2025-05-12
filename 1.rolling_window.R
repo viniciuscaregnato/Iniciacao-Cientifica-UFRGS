29418316a8566ae91f3ae9a14acf0f240a480527
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-05-12 14:56:55 -0300

    Update 1.rolling_window.R

--- 1.rolling_window.R
+++ 1.rolling_window.R
@@ -1,4 +1,4 @@
-rolling_window <- function(df, target_var, ART, horizon, n_predicts = 2, fnct) {
+rolling_window <- function(df, target_var, ART, horizon, n_predicts, fnct) {
   
   # n_predicts: numero de observações out-of-sample/numero de Xout
   
@@ -7,19 +7,26 @@ rolling_window <- function(df, target_var, ART, horizon, n_predicts = 2, fnct) {
   for (i in 0:(n_predicts - 1)) {
     
     
-    df_window <- df[(1 + i):(nrow(df) - n_predicts + i), ]  # pega do primeiro Xin ate o ultimo Yin de cada "i+1"° janela
+    df_window <- df[(1 + i):(nrow(df) - n_predicts + i), ]  # pega do primeiro Xin ate o ultimo Yin -  de cada (i+1)° janela
     
     
     resultado <- fnct(df_window, target_var, ART, horizon)
     
     
     previsoes[[i + 1]] <- resultado[[1]] 
+    names(previsoes) <- as.character((nrow(df) - n_predicts + 1):nrow(df)) # informa o idice do Yout (predito)
+    
     
     
   }
   
+  previsoes <- unlist(previsoes)
+  
+  lista_obs <- tail(df[[target_var]], n_predicts)
+  names(lista_obs) <- as.character((nrow(df) - n_predicts + 1):nrow(df))
   
   
-  
-  return(previsoes)
+  return(list(preditos = previsoes, observados = lista_obs ))
 }
+
+
