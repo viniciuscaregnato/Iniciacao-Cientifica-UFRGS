bb2f1d37026473eae87a6e879fc15e2edb506adc
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-05-20 15:27:04 -0300

    Add files via upload

---
+++ 6.RIDGE.R
@@ -0,0 +1,19 @@
+RIDGE <- function(df, target_var, ART, horizon, alpha = 0){
+  
+  require(glmnet)
+  
+  prep_data <- dataprep(df, target_var, ART, horizon)
+  
+  Xin <- as.matrix(prep_data$Xin)
+  Yin <- as.matrix(prep_data$Yin)
+  Xout <- as.matrix(prep_data$Xout)
+  
+  # RIDGE aplicado por cross validation
+  modelo <- cv.glmnet(Xin,Yin, type.measure = "mse", alpha = alpha, family="gaussian")
+  
+  previsao <- predict(modelo, newx = Xout)
+  
+  return(previsao = previsao)
+  
+  
+}
