acb964a9d8928594451a8c2537d7a377be138389
viniciuscaregnato <vinicius.cgarcia@gmail.com>
2025-05-20 15:33:40 -0300

    Add files via upload

---
+++ 5.lasso.R
@@ -0,0 +1,19 @@
+LASSO <- function(df, target_var, ART, horizon, alpha = 1){
+  
+  require(glmnet)
+  
+  prep_data <- dataprep(df, target_var, ART, horizon)
+  
+  Xin <- as.matrix(prep_data$Xin)
+  Yin <- as.matrix(prep_data$Yin)
+  Xout <- as.matrix(prep_data$Xout)
+  
+  # lasso definindo coeficientes atraves de validação cruzada
+  
+  modelo <- cv.glmnet(Xin,Yin, type.measure = "mse", alpha = alpha, family = "gaussian" )
+  
+  previsao <- predict(modelo, newx = Xout)
+  
+  
+}
+
